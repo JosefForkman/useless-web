e4c6d4b1ed118917bc950910358eac7bc7d1df2b
JosefForkman <strand.vatten@outlook.com>
2022-12-04 18:03:16 +0100

    small changes

--- dice/js/script.js
+++ dice/js/script.js
@@ -1,13 +1,57 @@
-const dice = document.querySelector('.dice');
-let count = 1;
-
+const main = document.querySelector('.continer');
+const range = document.querySelector('input')
 // setInterval( () => {
 //     dice.dataset.side = randomNumber(1, 7);
 // }, 500)
 
+class Dice {
+    constructor (side = this.randomNumber(1, 7)) {
+        this.sida = side;
+        this.element;
 
-dice.addEventListener('click', () => dice.dataset.side = randomNumber(1, 7))
+        this.init();
+    }
 
-function randomNumber(min, max) {
-    return Math.floor(Math.random()  * (max - min) + min);
+    /* Create dice element */
+    init() {
+        const dice = document.createElement('div');
+        dice.classList.add('dice');
+        dice.dataset.side = this.sida;
+
+        /* Events */
+        dice.addEventListener('click', () => dice.dataset.side = this.randomNumber(1, 7))
+
+        for (let i = 0; i < 6; i++) {
+            const dot = document.createElement('span');
+
+            dice.appendChild(dot);
+        }
+
+        this.element = dice;
+    }
+
+    /* Draw dice element to canvas */
+    draw(container) {
+        container.appendChild(this.element)
+    }
+
+    randomNumber(min, max) {
+        return Math.floor(Math.random()  * (max - min) + min);
+    }
 }
+
+/* Create dice of Dice class */
+let dices = []
+for (let i = 0; i < 8*20; i++) {
+    const dice = new Dice();
+    dices.push(dice);
+    dice.draw(main)
+}
+
+console.table(dices);
+
+
+range.addEventListener('input', e => {
+    console.log(e)
+})
+
